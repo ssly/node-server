70893a3efb7848648eb3395253de803ec5cd598f
liuyang <34771695@qq.com>
2017-09-03 22:05:18 +0800

    Add home page /works

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ if (require('./assets/common').sslEnable) {
 }
 
 // uncomment after placing your favicon in /dist
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public/assets', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
